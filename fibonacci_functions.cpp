0e3f86220292a4b754e251d0591c8b9cc285fe96
damcha02 <danil.poluyanov13@gmail.com>
2025-03-26 17:14:42 +0100

    added vector naive approach

--- fibonacci_functions.cpp
+++ fibonacci_functions.cpp
@@ -1,5 +1,19 @@
 #include "fibonacci_functions.hpp"
 
+using namespace std::chrono_literals;
+
+// global variables
+bool global_excede_time = false;  // actual definition
+
+//timer to time the functions as a thread
+static bool done = false;
+void timer(){
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::cout << "timer has finished" << std::endl;
+    done = true;
+}
+
+
 unsigned long long recursive_fb(unsigned long n){
     if(n < 2) return n;
     return recursive_fb(n - 1) + recursive_fb(n - 2);
@@ -20,4 +34,72 @@ unsigned long long naive_fb(long unsigned n){
         num2 = result;
     }
     return result;
+}
+std::vector<unsigned> vector_addition(std::vector<unsigned>& num1, std::vector<unsigned>& num2){
+    // num2 should always be bigger or equal to num1 because it is always the next number in the sequence
+    assert(num1.size() <= num2.size()); 
+
+    std::vector<unsigned> result;
+    result.reserve(num2.size() + 1);
+    int carry = 0;
+    for(int i = 0; i < num2.size(); ++i){
+        //prevent accessing num1 out of bounds
+        if(i < num1.size()){
+            if(num1.at(i) + num2.at(i) + carry < 10){
+                result.push_back(num1.at(i) + num2.at(i) + carry);
+                carry = 0; // carry in either case going to be 0, wether it was or wasnt already 0
+            }
+            if(num1.at(i) + num2.at(i) + carry >= 10){
+                result.push_back( (num1.at(i) + num2.at(i) + carry) % 10 );
+                carry = 1; // carry in either case going to be 1, wether it was or wasnt already 1
+            }
+        }
+        else{
+            if(num2.at(i) + carry < 10){
+                result.push_back( num2.at(i) + carry );
+                carry = 0;
+            }
+            if(num2.at(i) + carry >= 10){
+                result.push_back(num2.at(i) + carry);
+                carry = 1;
+            }
+        }
+    }
+    if(carry == 1){
+        result.push_back(carry);
+    }
+    num1 = num2;
+    num2 = result;
+    //maybe std::swap?
+    return result;
+}
+
+
+std::vector<unsigned> vectorization_fb(long unsigned n){
+    std::vector<unsigned> num1, num2, result;
+    num1.push_back(0);
+    num2.push_back(1);
+    if(n == 0) return num1;
+    if(n == 1) return num2;
+    
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_vec_appr, end_vec_appr;
+    std::chrono::duration<double> duration_per_fibonacci_vec_appr = 0s;
+
+    start_vec_appr = std::chrono::high_resolution_clock::now();
+    for(int i = 2; i <= n; ++i){
+        result = vector_addition(num1, num2);
+    }
+    end_vec_appr = std::chrono::high_resolution_clock::now();
+
+    duration_per_fibonacci_vec_appr = end_vec_appr - start_vec_appr;
+    if(duration_per_fibonacci_vec_appr >= 1s){
+        global_excede_time = true;
+        std::cout << "finished after: " << duration_per_fibonacci_vec_appr.count() << "s";
+        return num1;
+    }
+    return result;
+}
+
+void print(std::vector<unsigned> vec){
+    std::for_each(vec.rbegin(), vec.rend(), [](unsigned &n){std::cout << n;});
 }
--- fibonacci_functions.hpp
+++ fibonacci_functions.hpp
@@ -5,10 +5,23 @@
 #include <ctime>
 #include <thread>
 #include <climits>
+#include <vector>
+#include <assert.h>
+#include <algorithm>
 
-
+extern bool global_excede_time;
 
 unsigned long long recursive_fb(unsigned long n);
 
 
 unsigned long long naive_fb(long unsigned n);
+
+std::vector<unsigned> vector_addition(std::vector<unsigned>& num1, std::vector<unsigned>& num2);
+
+std::vector<unsigned> vectorization_fb(long unsigned n);
+
+void print(std::vector<unsigned> vec);
+
+// void test_recursive();
+
+// void test_naive();
